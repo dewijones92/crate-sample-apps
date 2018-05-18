9ce258c4573005345de2e7f6460d3fffa2c445e3
Lukas Bals <mail@lukasbals.at>
2018-05-18 16:46:30 +0200

    fix erlang added cors

---
+++ erlang/src/guestbook_cors.hrl
@@ -0,0 +1,32 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2016, CRATE Technology GmbH
+%%% Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+%%% license agreements.  See the NOTICE file distributed with this work for
+%%% additional information regarding copyright ownership.  Crate licenses
+%%% this file to you under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.  You may
+%%% obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+%%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+%%% License for the specific language governing permissions and limitations
+%%% under the License.
+%%%
+%%% However, if you have executed another commercial license agreement
+%%% with Crate these terms will supersede the license and you may use the
+%%% software solely pursuant to the terms of the relevant commercial agreement.
+%%%-------------------------------------------------------------------
+
+-export([
+  addCORSHeaders/1
+]).
+
+addCORSHeaders(Req) ->
+	Req2 = cowboy_req:set_resp_header(<<"access-control-allow-methods">>, <<"GET, POST, PUT, DELETE, OPTIONS">>, Req),
+  Req3 = cowboy_req:set_resp_header(<<"access-control-allow-origin">>, <<"*">>, Req2),
+  Req4 = cowboy_req:set_resp_header(<<"access-control-allow-headers">>, <<"Origin, X-Requested-With, Content-Type, Accept">>, Req3),
+  Req4.
+% Accept-Charset: utf-8, iso-8859-1;q=0.5
